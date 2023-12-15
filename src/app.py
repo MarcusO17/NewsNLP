d6458c93d98cc6e5a6d3416c0172fb13fe19c2ce
MarcusO17 <marcusonggeye@gmail.com>
2023-12-16 03:03:38 +0800

    test

--- src/app.py
+++ src/app.py
@@ -66,7 +66,7 @@ def gen_pie_chart(df,site):
 
  
 def app():
-    main_df = pd.read_csv('data/BBCNews.csv')
+    main_df = pd.read_csv('./data/BBCNews.csv')
 
     st.title("Narratives in Focus: Exploring Malaysia Through BBC's Lens")
 
